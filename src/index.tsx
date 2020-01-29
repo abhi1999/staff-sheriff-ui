51f01d63e859f5f89b82fa372c215d22cbf2ee4d
abhishek <abhi1999@gmail.com>
2020-01-29 13:13:15 -0500

    Added log rocket, instructions for variable override

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import LogRocket from 'logrocket';
+// LogRocket.init('9mzoy6/afd-poc');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
